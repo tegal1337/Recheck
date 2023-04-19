085e1cb4f0936790057b700aaf5d41c4776989fe
taqin <abdulmuttaqin456@gmail.com>
2023-04-19 17:40:15 +0700

    refactored

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { prompt } = require('enquirer');
-var clc = require("cli-color");
-const { ERR_DOMAIN, BANNER, QUESTION, VALIDATOR, INPUT, CAPTION , FAQ} = require('./lib/constant');
+const clc = require("cli-color");
+const fs = require("fs");
+const { ERR_DOMAIN, BANNER, QUESTION, VALIDATOR, INPUT, CAPTION , FAQ } = require('./lib/constant');
 const { reverseIP } = require('./lib/reverseip');
 const { checkStatus } = require('./lib/http-status');
-const fs = require("fs");
 
 const question = [
   {
@@ -15,42 +15,32 @@ const question = [
   }
 ];
 
-console.log(clc.greenBright(BANNER) + "\n" + clc.blueBright(CAPTION) + "\n" + clc.yellowBright(FAQ) + "\n");
+console.log(clc.greenBright(BANNER), clc.blueBright(CAPTION), clc.yellowBright(FAQ), "\n");
 
 async function start() {
-  let result = await prompt(question);
+  const result = await prompt(question);
   const res = await reverseIP(result.domain);
- 
+
   console.log(clc.greenBright("Found Link ") + " : " + clc.cyan(res.length) + "\n");
   fs.writeFileSync(`./result/${result.domain}.txt`, JSON.stringify(res), 'utf8');
-  for (let i = 0; i < res.length; i++) {
+
+  for (const url of res) {
     try {
-     
-      //check if url not include http://
-      if (res[i].indexOf("http://") === -1) {
-        res[i] = `http://${res[i]}`;
-      }
+      const formattedUrl = url.indexOf("http://") === -1 ? `http://${url}` : url;
+      const status = await checkStatus(formattedUrl);
 
-      const status =  await checkStatus(res[i]);
-    //  console.log(status)
       if (status) {
-        console.log(`${clc.bgBlueBright(res[i])} ${clc.bgGreen(clc.whiteBright("is possible to takeover"))}`);
-        // write to file
-        fs.appendFileSync(`./result/possible.txt`, `${res[i]}\n`, 'utf8');
-      } else{
-        console.log(`${clc.blueBright(res[i]) + " : " + status} ${clc.bgRed(clc.whiteBright("is alive and it's not possible to takeover"))}`);
+        console.log(`${clc.bgBlueBright(formattedUrl)} ${clc.bgGreen(clc.whiteBright("is possible to takeover"))}`);
+        fs.appendFileSync(`./result/possible.txt`, `${formattedUrl}\n`, 'utf8');
+      } else {
+        console.log(`${clc.blueBright(formattedUrl) + " : " + status} ${clc.bgRed(clc.whiteBright("is alive and it's not possible to takeover"))}`);
       }
-
     } catch (error) {
       console.log(clc.red(error));
     }
 
-    await new Promise(resolve => { setTimeout(resolve, 1000) });
+    await new Promise(resolve => setTimeout(resolve, 1000));
   }
 }
 
 start();
-
-
-
-
